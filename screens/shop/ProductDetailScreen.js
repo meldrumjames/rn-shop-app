a4b93215113730b67ce432fe9708dedc727e23b0
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-02-27 17:51:18 +0000

    overview screen basics

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+} from "react-native";
+
+const ProductDetailScreen = (props) => {
+  return <ScrollView></ScrollView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductDetailScreen;
