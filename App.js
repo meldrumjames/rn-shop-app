5ebe49ecf246bde3eee2f256700c6a01ba664c38
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-03-13 15:05:31 +0000

    added orders screen and clearing cart

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
 import ShopNavigator from "./navigation/ShopNavigator";
 
 LogBox.ignoreLogs([
@@ -17,6 +18,7 @@ LogBox.ignoreLogs([
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(rootReducer);
---
+++ screens/shop/CartSCreen.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+
+import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
+import * as CartActions from "../../store/actions/cart";
+import * as OrderActions from "../../store/actions/orders";
+
+const CartScreen = (props) => {
+  const cartTotal = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productID: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
+    }
+    return transformedCartItems.sort((a, b) =>
+      a.productID > b.productID ? 1 : -1
+    );
+  });
+
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summaryContainer}>
+        <Text style={styles.summaryText}>
+          Total: <Text style={styles.priceText}>${cartTotal.toFixed(2)}</Text>
+        </Text>
+        <Button
+          color={Colors.accent}
+          title="Order"
+          disabled={cartItems.length === 0}
+          onPress={() => {
+            dispatch(OrderActions.addOrder(cartItems, cartTotal));
+          }}
+        />
+      </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={(item) => item.productID}
+        renderItem={(itemData) => (
+          <CartItem
+            quantity={itemData.item.quantity}
+            title={itemData.item.productTitle}
+            amount={itemData.item.sum}
+            onRemove={() => {
+              dispatch(CartActions.removeFromCart(itemData.item.productID));
+            }}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20,
+  },
+  summaryContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
+  summaryText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+  },
+  priceText: {
+    color: Colors.primary,
+  },
+});
+
+export default CartScreen;
